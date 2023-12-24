621b7b143597226de6ecaa56191e868bf4f547c9
vdt2208 <vudinhthinh22082003@gmail.com>
2023-12-24 12:29:43 +0700

    All-Admin

--- Shop_Bear/Areas/Admin/Controllers/ProductImageController.cs
+++ Shop_Bear/Areas/Admin/Controllers/ProductImageController.cs
@@ -14,8 +14,29 @@ namespace Shop_Bear.Areas.Admin.Controllers
 		}
 		public ActionResult Index(int id)
 		{
+			ViewBag.ProductId = id;
 			var items = _context.ProductImages.Where(x => x.ProductId == id).ToList();
-			return View();
+			return View(items);
+		}
+        [HttpPost]
+        public ActionResult AddImage(int productId, string url)
+		{
+			_context.ProductImages.Add(new Models.EF.ProductImage
+			{
+				ProductId = productId,
+				Image = url,
+				IsDefault = false
+			});
+			_context.SaveChanges();
+			return Json(new { Success = true });
+		}
+		
+		public ActionResult Delete(int id)
+		{
+			var item = _context.ProductImages.Find(id);
+			_context.ProductImages.Remove(item);
+			_context.SaveChanges();
+			return Json(new {success =true });
 		}
 	}
 }
